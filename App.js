d8598cb1ea54fca193cd4a20dadb8f4c59e853bd
stanley-c-so <stanley.c.so@gmail.com>
2019-07-03 18:12:38 -0400

    [FEAT] improved crosshair UI

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Provider, connect } from 'react-redux';
-import { Platform, StyleSheet, Text, View, Dimensions, YellowBox } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  YellowBox
+} from 'react-native';
 
 console.ignoredYellowBox = ['Remote debugger'];
 YellowBox.ignoreWarnings([
@@ -34,7 +41,13 @@ const Game = props => (
       debug={true}
     />
     {/* crosshair is its own view, following a stylesheet */}
-    <View style={styles.crosshair} />
+    <View style={{ ...styles.crosshairV1, top: styles.crosshairV1.top - 50, left: styles.crosshairV1.left - 1 }} />
+    <View style={{ ...styles.crosshairH1, top: styles.crosshairV1.top - 1, left: styles.crosshairV1.left - 50 }} />
+    <View style={{ ...styles.crosshairV2, top: styles.crosshairV2.top - 50, left: styles.crosshairV2.left - 2 }} />
+    <View style={{ ...styles.crosshairV2, top: styles.crosshairV2.top + 25, left: styles.crosshairV2.left - 2 }} />
+    <View style={{ ...styles.crosshairH2, top: styles.crosshairV2.top - 2, left: styles.crosshairV2.left - 50 }} />
+    <View style={{ ...styles.crosshairH2, top: styles.crosshairV2.top - 2, left: styles.crosshairV2.left + 25 }} />
+    <View style={styles.crosshairCircle} />
   </View>
 );
 
@@ -139,14 +152,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
-  crosshair: {
+  crosshairCircle: {
     position: 'absolute',
-    top: Dimensions.get('window').height / 2 - 2,
-    left: Dimensions.get('window').width / 2 - 2,
+    top: Dimensions.get('window').height / 2 - 50,
+    left: Dimensions.get('window').width / 2 - 50,
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    borderWidth: 4,
+    borderColor: '#a10d0d'
+  },
+  crosshairV1: {
+    position: 'absolute',
+    top: Dimensions.get('window').height / 2,
+    left: Dimensions.get('window').width / 2,
+    height: 100,
+    width: 2,
+    backgroundColor: '#a10d0d'
+  },
+  crosshairH1: {
+    position: 'absolute',
+    top: Dimensions.get('window').height / 2,
+    left: Dimensions.get('window').width / 2,
+    height: 2,
+    width: 100,
+    backgroundColor: '#a10d0d'
+  },
+  crosshairV2: {
+    position: 'absolute',
+    top: Dimensions.get('window').height / 2,
+    left: Dimensions.get('window').width / 2,
+    height: 25,
     width: 4,
+    backgroundColor: '#a10d0d'
+  },
+  crosshairH2: {
+    position: 'absolute',
+    top: Dimensions.get('window').height / 2,
+    left: Dimensions.get('window').width / 2,
     height: 4,
-    borderRadius: 2,
-    borderWidth: 1,
-    backgroundColor: 'red'
-  }
+    width: 25,
+    backgroundColor: '#a10d0d'
+  },
 });
