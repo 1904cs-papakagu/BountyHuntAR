5259864defbf362343030babb52aeb6f63b627be
Kevin Lam <find.keviin@gmail.com>
2019-07-06 15:56:51 -0400

    [BUGFIX] Fixed oversized image in winner/loser screen on Andriod devices

--- js/Comps/UI/EndScreen.js
+++ js/Comps/UI/EndScreen.js
@@ -1,74 +1,83 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions, Image } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Image
+} from 'react-native';
 import { resetStatus } from '../../store/';
 import { connect } from 'react-redux';
 
-
-const EndScreen = (props) => (
-    <View style={styles.container}>
-        <Text style={styles.banner}>
-            {props.status === 'won' ?
-                <Image
-                source={require('../../Images/winner.png')}
-                style={styles.logoImg}
-                resizeMethod="scale"
-              /> :
-                ''}
-            {props.status === 'lost' ?
-                <Image
-                source={require('../../Images/loser.png')}
-                style={styles.logoImg}
-                resizeMethod="scale"
-              /> :
-                ''}
-        </Text>
-
-
-        <TouchableOpacity
-            onPress={() => props.ok()}
-        >
-        <Text style={styles.button}>Back to profile</Text>
-        </TouchableOpacity>
+const EndScreen = props => (
+  <View style={styles.container}>
+    <View style={styles.imageContainer}>
+      {props.status === 'won' ? (
+        <Image
+          source={require('../../Images/winner.png')}
+          style={styles.logoImg}
+          resizeMethod="scale"
+        />
+      ) : (
+        <></>
+      )}
+      {props.status === 'lost' ? (
+        <Image
+          source={require('../../Images/loser.png')}
+          style={styles.logoImg}
+          resizeMethod="scale"
+        />
+      ) : (
+        <></>
+      )}
     </View>
-)
-
+      <TouchableOpacity onPress={() => props.ok()}>
+        <Text style={styles.button}>Back to profile</Text>
+      </TouchableOpacity>
+  </View>
+);
 
 const mapDispatchToProps = dispatch => {
-    return {
-        ok() {
-            dispatch(resetStatus());
-        }
-    };
+  return {
+    ok() {
+      dispatch(resetStatus());
+    }
+  };
 };
 
-export default connect(null, mapDispatchToProps)(EndScreen)
-
+export default connect(
+  null,
+  mapDispatchToProps
+)(EndScreen);
 
 let { height, width } = Dimensions.get('window');
 const styles = StyleSheet.create({
-    container: {
-        height: height,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#000000'
-    },
-    banner: {
-        fontFamily: 'American Typewriter',
-        fontSize: 30,
-        color: '#f54242'
-    },
-    button: {
-        backgroundColor: 'black',
-        borderColor: 'red',
-        borderWidth: 1,
-        borderRadius: 12,
-        color: 'white',
-        margin: 10,
-        padding: 12,
-        textAlign: 'center'
-      },
-    logoImg: {
-        width: width,
-        resizeMode: 'contain'
-      }
+  container: {
+    width: width,
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000'
+  },
+  imageContainer: {
+    width: width,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000000'
+  },
+  button: {
+    backgroundColor: 'black',
+    borderColor: 'red',
+    borderWidth: 1,
+    borderRadius: 12,
+    color: 'white',
+    margin: 10,
+    padding: 12,
+    textAlign: 'center'
+  },
+  logoImg: {
+    width: width,
+    resizeMode: 'contain'
+  }
 });
