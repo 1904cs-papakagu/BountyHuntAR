e1cf586c7912aba39c8377c3e18ee06e9ac2809a
EricBot89 <eloucks@gmail.com>
2019-07-08 10:44:15 -0400

    [BUGFIX] fixed missing func bug

--- js/store/reducers/game.js
+++ js/store/reducers/game.js
@@ -3,10 +3,12 @@ const initState = {status: '', agents: {}}
 const START_PLAYING = "START_PLAYING"
 const STOP_PLAYING = "STOP_PLAYING"
 const UPDATE_AGENT = 'UPDATE_AGENT'
+const RESET = 'RESET'
 
 export const updateAgent = (agentId,agentPosition) => {return{type:UPDATE_AGENT, agentId, agentPosition}}
 export const startGame = (locationId,userId,displacement) => {return{type: START_PLAYING, locationId, userId ,displacement}}
 export const endGame = (won) => {return{type: STOP_PLAYING, won}}
+export const resetStatus = () => {return{type: RESET}}
 export const spookTarget = () => {}
 
 
@@ -24,8 +26,9 @@ export default function(state = initState, action){
             const {agentId, agentPosition} = action
             state.agents[agentId] = agentPosition
             console.log(state)
-
             return state
+        case RESET:
+            return initState
         default:
             break;
     }
