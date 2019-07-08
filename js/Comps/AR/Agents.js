1f8f9c01e3f740431a892f87880d8ad69077a5be
EricLoucks <35211750+EricBot89@users.noreply.github.com>
2019-07-08 15:56:10 -0400

    Update Agents.js

--- js/Comps/AR/Agents.js
+++ js/Comps/AR/Agents.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ViroBox, Viro3DObject } from 'react-viro';
 
 const Agents = props => {
-  // console.log('AGENT POSITION:', [agent[0] + props.displacement[0], 0, agent[2] + props.displacement[1]]);
   const agent = props.agent;
   const displacement = props.displacement;
   return (
